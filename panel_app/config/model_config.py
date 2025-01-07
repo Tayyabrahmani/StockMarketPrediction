380fecac090b2c49ca2f3596415d250f3880dde4
Tayyab Rahmani <tayyabrahmani@gmail.com>
2025-01-07 22:38:43 +0100

    fix: change available models

--- panel_app/config/model_config.py
+++ panel_app/config/model_config.py
@@ -1,1 +1,1 @@
-AVAILABLE_MODELS = ["ARIMA", "XGBoost", "SARIMA", "Prophet", "LSTM", "GRU", "MLP", "CNN", "RNN", "SVR", "Transformers"]
+AVAILABLE_MODELS = ["ARIMA", "XGBoost", "CNN", "LSTM", "GRU", "MLP", "RNN", "SVR", "Transformers"]
